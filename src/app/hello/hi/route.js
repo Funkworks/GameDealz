d183aa98c71e0b10a4910d3e592d6ec96d3620da
sicfran774 <sicfran.774@gmail.com>
2023-09-19 23:25:05 -0700

    App Router testing

--- src/app/hello/hi/route.js
+++ src/app/hello/hi/route.js
@@ -2,6 +2,6 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   return NextResponse.json({ 
-    names: ['Juniper Watson', 'Francis Rodriguez'] 
+    message: "Sup"
   });
 }
