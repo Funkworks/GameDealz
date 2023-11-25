5450c946d6c7e6d06170acc08560633ded7346b6
Sulfurous-Impersonation <june@watson-net.com>
2023-11-25 14:21:26 -0800

    Added email alert management methods

---
+++ src/app/emails.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+//given a game's gameID
+export async function setAlert(gameID, email)
+{
+	//get game's retail price
+	const priceResponse = await axios.get(`https://www.cheapshark.com/api/1.0/games?id=${gameID}`);
+	const price = parseInt(priceResponse.deals.retailPrice);
+
+	//set threshold price to wait for to alert user
+	const threshold = price * 0.85; //15% off
+
+	//set alert
+	const alertResponse = await axios.get(`https://www.cheapshark.com/api/1.0/alerts?action=set&email=${email}&gameID=${gameID}&price=${threshold}`);
+	return alertResponse;
+}
+
+export async function getAlert(email)
+{
+	const manageResponse = await axios.get(`https://www.cheapshark.com/api/1.0/alerts?action=manage&email=${email}`);
+	return manageResponse;
+}
+
+export async function deleteAlert(gameID, email)
+{
+	const deleteResponse = await axios.get(`https://www.cheapshark.com/api/1.0/alerts?action=delete&email=${email}&gameID=${gameID}&price=0.0`);
+	return deleteResponse;
+}
