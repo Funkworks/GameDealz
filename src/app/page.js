222d7227a12159e6615cd2058bad8ceb6fdd9f12
Sulfurous-Impersonation <june@watson-net.com>
2023-11-29 11:41:06 -0800

    Fixed merge error

--- src/app/page.js
+++ src/app/page.js
@@ -2,51 +2,6 @@ import styles from "./page.module.css";
 import Image from "next/image";
 
 export default function Home() {
-
-  const [results, setResults] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [user, setUser] = useState(null);
-  const [username, setUsername] = useState("")
-
-  // This useEffect() runs at the beginning of page render because of the [] at the end
-  useEffect(() => {
-    UserLoggedIn();
-  }, [])
-
-  const UserLoggedIn = async () => {
-    try{
-      const { data: { user } } = await supabase.auth.getUser()
-      const { data, error } = await supabase
-        .from('users')
-        .select(`username`)
-        .eq('email', user.email)
-      setUsername(data[0].username)
-      setUser(user)
-    } catch (e) {
-      console.log("User not signed in")
-    }
-  }
-
-  const SignOut = async () => {
-    const { error } = await supabase.auth.signOut()
-    window.location.reload();
-  }
-
-  const GameSearch = async (query) => {
-    setSearchQuery(query);
-    setLoading(true);
-    try {
-      const response = await axios.get(
-        `https://www.cheapshark.com/api/1.0/deals?title=${query}`
-      );
-      setResults(response.data);
-    } catch (error) {
-      console.error("Failed to fetch data", error);
-    }
-    setLoading(false);
-  };
-
   return (
     <main>
       <body>
